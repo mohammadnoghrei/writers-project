c001c47582bc30b5ac81bebc9622ea5b22152b75
mohammadnoghrei <noghreimohammad@gmail.com>
2024-01-09 00:20:03 +0330

    add books id field in books class and add a constractor , get&set method and fix to string

--- src/main/java/org/example/BookRepository.java
+++ src/main/java/org/example/BookRepository.java
@@ -11,11 +11,12 @@ public class BookRepository {
     }
     public void save(Books books) throws SQLException {
         Connection connection = jdbcconnection.getConnection();
-        String savebook = "insert into books(?,?,?) ";
+        String savebook = "insert into books(bookName,year,writerid)\n" +
+                "values (?,?,?);";
         PreparedStatement preparedStatement = connection.prepareStatement(savebook);
         preparedStatement.setString(1,books.getTitle());
         preparedStatement.setInt(2,books.getYear());
         preparedStatement.setInt(3,books.getWriter_id());
-
+        preparedStatement.executeUpdate();
     }
 }
