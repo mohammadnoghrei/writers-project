21b53ea03397d8150d54af3fc5b1c240318c1cf5
mohammadnoghrei <noghreimohammad@gmail.com>
2024-01-08 23:48:24 +0330

    create save method for BookRepository class

--- src/main/java/org/example/BookRepository.java
+++ src/main/java/org/example/BookRepository.java
@@ -1,4 +1,20 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class BookRepository {
+    jdbcconnection jdbcconnection =new jdbcconnection();
+
+    public BookRepository() throws SQLException {
+    }
+    public void save(Books books) throws SQLException {
+        Connection connection = jdbcconnection.getConnection();
+        String savebook = "insert into books(?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(savebook);
+        preparedStatement.setString(1,"a");
+        preparedStatement.setInt(2,1992);
+        preparedStatement.setInt(3,1);
+    }
 }
