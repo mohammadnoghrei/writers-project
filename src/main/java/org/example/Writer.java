e3b94fc1e54fdce3df3c760876db70fe4ecae4a8
mohammadnoghrei <noghreimohammad@gmail.com>
2024-01-08 22:22:19 +0330

    create constractors and toString method for Writer class and get&set method for fields

--- src/main/java/org/example/Writer.java
+++ src/main/java/org/example/Writer.java
@@ -1,9 +1,80 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Writer {
     int writer_id;
     String first_name;
     String last_name;
     int age;
     String[] books;
+
+    public Writer() {
+    }
+
+    public Writer(int writer_id, String first_name, String last_name, int age, String[] books) {
+        this.writer_id = writer_id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.age = age;
+        this.books = books;
+    }
+
+    public Writer(String first_name, String last_name, int age, String[] books) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.age = age;
+        this.books = books;
+    }
+
+    public int getWriter_id() {
+        return writer_id;
+    }
+
+    public void setWriter_id(int writer_id) {
+        this.writer_id = writer_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String[] getBooks() {
+        return books;
+    }
+
+    public void setBooks(String[] books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Writer{" +
+                "writer_id=" + writer_id +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", age=" + age +
+                ", books=" + Arrays.toString(books) +
+                '}';
+    }
 }
