3a3d4010a4841322c6c8b276d10a8456066bb325
mohammadnoghrei <noghreimohammad@gmail.com>
2024-01-09 16:46:41 +0330

    create save method for WriterRepository

--- src/main/java/org/example/WriterRepository.java
+++ src/main/java/org/example/WriterRepository.java
@@ -1,4 +1,27 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class WriterRepository {
+    private final jdbcconnection jdbcconnection =new jdbcconnection();
+
+    public WriterRepository() throws SQLException {
+    }
+    public void save(Writer writer) throws SQLException {
+        Connection connection = jdbcconnection.getConnection();
+        String savebook = "insert into writers(FirstName,LastName,age)\n" +
+                "values (?,?,?);";
+        PreparedStatement preparedStatement = connection.prepareStatement(savebook);
+        preparedStatement.setString( 1,writer.getFirst_name());
+        preparedStatement.setString(2,writer.getLast_name());
+        preparedStatement.setInt(3, writer.getAge());
+        preparedStatement.executeUpdate();
+        System.out.println(preparedStatement.executeUpdate());
+    }
+
+//    public Writer load(int writerid){
+//
+//    }
 }
