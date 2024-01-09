225f911680e56e2bc3d7ab125172b8664c1dbe35
mohammadnoghrei <noghreimohammad@gmail.com>
2024-01-09 17:44:05 +0330

    create load method for books

--- src/main/java/org/example/BookRepository.java
+++ src/main/java/org/example/BookRepository.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BookRepository {
@@ -18,5 +19,23 @@ public class BookRepository {
         preparedStatement.setInt(2,books.getYear());
         preparedStatement.setInt(3, books.getWriter_id());
         preparedStatement.executeUpdate();
+    }
+
+    public Books load(int bookid) throws SQLException {
+        Connection connection =jdbcconnection.getConnection();
+        String select = "SELECT * FROM books WHERE book_id= ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(select);
+        preparedStatement.setInt(1,bookid);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()){
+            int book_id = resultSet.getInt("book_id");
+            String bookName= resultSet.getString("bookName");
+            int year = resultSet.getInt("year");
+            int writerid = resultSet.getInt("writerid");
+            Books books = new Books(book_id,bookName,year,writerid);
+            return books;
+        }else System.out.println("your book id not exist");
+        return null;
+
     }
 }
