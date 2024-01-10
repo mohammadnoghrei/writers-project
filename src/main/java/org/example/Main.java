2df2e880e529feb7f7f2099782920e513ad4c400
mohammadnoghrei <noghreimohammad@gmail.com>
2024-01-10 23:57:28 +0330

    call methods in main and test it

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.sql.SQLException;
+import java.util.Date;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        System.out.println("*******welcome*******");
+        WriterService.register();
+        System.out.println(" ");
+        System.out.println("********************");
+        BookService.addbook();
+
+
 
     }
 }
